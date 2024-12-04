5b78fa5d957a9be951b2060aed2dcdd7adf4b3d8
Animalien <Animalien@users.noreply.github.com>
2024-12-04 09:41:42 -0800

    Problem 4, part 2

--- Problem4.cpp
+++ Problem4.cpp
@@ -38,6 +38,7 @@ private:
         static const BigInt numDirs = xInc.size();
 
         BigInt foundWordCount = 0;
+        BigInt foundFormationCount = 0;
 
         for (BigInt y = 0; y <= maxY; ++y)
         {
@@ -45,43 +46,84 @@ private:
             {
                 for (BigInt dir = 0; dir < numDirs; ++dir)
                 {
+                    //
+                    // Find XMAS word
+                    // 
+
                     // can the word even fit in this direction
 
                     const BigInt xEnd = x + (wordToFindLenStep * xInc[dir]);
-                    if ((xEnd < 0) || (xEnd > maxX))
-                        continue;
-
                     const BigInt yEnd = y + (wordToFindLenStep * yInc[dir]);
-                    if ((yEnd < 0) || (yEnd > maxY))
-                        continue;
 
-                    // now test letters
-
-                    bool foundWord = true;
-                    for (BigInt letter = 0; letter < wordToFindLen; ++letter)
+                    if ((xEnd >= 0) && (xEnd <= maxX) && (yEnd >= 0) && (yEnd <= maxY))
                     {
-                        const BigInt xTest = x + (letter * xInc[dir]);
-                        const BigInt yTest = y + (letter * yInc[dir]);
+                        // now test letters
 
-                        if (lines[yTest][xTest] != wordToFind[letter])
+                        bool foundWord = true;
+                        for (BigInt letter = 0; letter < wordToFindLen; ++letter)
                         {
-                            foundWord = false;
-                            break;
+                            const BigInt xTest = x + (letter * xInc[dir]);
+                            const BigInt yTest = y + (letter * yInc[dir]);
+
+                            if (lines[yTest][xTest] != wordToFind[letter])
+                            {
+                                foundWord = false;
+                                break;
+                            }
+                        }
+
+                        if (foundWord)
+                        {
+                            ++foundWordCount;
+
+                            if (verbose)
+                                printf("  found word at %lld,%lld going in dir %lld\n", x, y, dir);
                         }
                     }
+                }
 
-                    if (foundWord)
+                //
+                // Find X-MAS formation
+                //
+
+                if ((x == 0) || (x == maxX) || (y == 0) || (y == maxY))
+                    continue;
+                if (lines[y][x] != 'A')
+                    continue;
+
+                BigUInt mask = 0;
+                bool bad = false;
+                for (BigInt dir = 1; dir < numDirs; dir += 2)
+                {
+                    mask <<= 1;
+
+                    const char ch = lines[y + yInc[dir]][x + xInc[dir]];
+                    if (ch == 'S')
                     {
-                        ++foundWordCount;
-
-                        if (verbose)
-                            printf("  found word at %lld,%lld going in dir %lld\n", x, y, dir);
+                        mask = mask | 1;
+                    }
+                    else if (ch != 'M')
+                    {
+                        bad = true;
+                        break;
                     }
                 }
+
+                if (bad)
+                    continue;
+
+                // mask can be:  0011, 0110, 1100, or 1001
+                if ((mask != 3) && (mask != 6) && (mask != 12) && (mask != 9))
+                    continue;
+
+                ++foundFormationCount;
+
+                if (verbose)
+                    printf("  found formation at %lld,%lld\n", x, y);
             }
         }
 
-        printf("  Total num times found word = %lld\n\n", foundWordCount);
+        printf("  Total num times found word = %lld, total num times found formation = %lld\n\n", foundWordCount, foundFormationCount);
     }
 };
 
